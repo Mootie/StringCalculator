afec1f114953040787fb7715f895528032b0991d
mootie@xor.net <mootie@xor.net>
2018-08-23 20:18:41 -0700

    Create string calculator add function. [Requirement 1]

---
+++ StringCalculator/StringCalculator - Copy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            List<string> delimiters = new List<string> { "\n", "," };
+            string[] tokens = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = 0;
+            foreach (string token in tokens)
+            {
+                int number = Int32.Parse(token);
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
---
+++ StringCalculatorUT/AddTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using StringCalculator;
+
+namespace StringCalculatorUT
+{
+    public class AddTest
+    {
+        [Fact]
+        public void EmptyString()
+        {
+            Assert.Equal(Calculator.Add(""), 0);
+        }
+
+        [Fact]
+        public void OneNumber()
+        {
+            Assert.Equal(Calculator.Add("1"), 1);
+        }
+
+        [Fact]
+        public void TwoNumbers()
+        {
+            Assert.Equal(Calculator.Add("1,2"), 3);
+        }
+    }
+}
